d90a866cb0cae059e92295b08b391f3d85c136de
root <lseirina87@gmail.com>
2025-07-05 12:21:18 +0300

    Added user app

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from drf_spectacular.views import (
-    SpectacularApiView,
+    SpectacularAPIView,
     SpectacularSwaggerView,
 )
 
@@ -23,7 +23,10 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularApiView.as_view(), name='api-schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api_schema'),
-         name='api-docs')
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
 ]
