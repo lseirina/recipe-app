85e496efb3e460db8a3b677c62f6bcbc926efae8
root <lseirina87@gmail.com>
2025-07-21 15:02:19 +0300

    Added test user need auhentication

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -91,12 +92,18 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        
+
     def test_create_token_bland_password(self):
         """Test posting a bland password return an error."""
         payload = {'email': 'test@example.com', 'password': ''}
-        
+
         res = self.client.post(TOKEN_URL, payload)
-        
+
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_retrieve_user_unauthorized(self):
+        """Test authentication is requered for user"""
+        res = self.client.get(ME_URL)
+        
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
