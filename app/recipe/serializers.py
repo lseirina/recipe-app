86ea84741287ed1cf9d7779c99297c18d9eb90ae
root <lseirina87@gmail.com>
2025-07-23 13:23:20 +0300

    Added RecipeSerializer

---
+++ app/recipe/serializers.py
@@ -0,0 +1,15 @@
+"""
+Serializer for recipe API.
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'links']
+        read_only=['id']
