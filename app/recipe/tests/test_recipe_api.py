3099037579f55edb112e74ded8237a48dfbbbcd5
root <lseirina87@gmail.com>
2025-07-23 13:06:48 +0300

    Added test recipe limited to auth user

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -53,7 +53,7 @@ class PrivateRecipeTests(TestCase):
             'test@example.com',
             'testpass123',
         )
-        self.client.force_authenticate(user=self.user)
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_recipes(self):
         """Test retrieve list of recipes is success."""
@@ -62,9 +62,23 @@ class PrivateRecipeTests(TestCase):
 
         res = self.client.get(RECIPES_URL)
         recipes = Recipe.objects.all().order_by('-id')
-        serializer = RecipeSerializer(recipes)
+        serializer = RecipeSerializer(recipes, many=True)
         
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-        
-        
+
+    def test_recipe_limited_to_user(self):
+        """Test recipes is limited to authenticated user."""
+        other_user = get_user_model().objects.create_user(
+            'other@example.com',
+            'pass2345',
+        )
+        create_recipe(other_user)
+        create_recipe(self.user)
+
+        res = self.client.get(RECIPES_URL)
+        recipes = Recipe.objects.filter(user=self.user)
+        serializer = RecipeSerializer(recipes)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
