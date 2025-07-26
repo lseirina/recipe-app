fd57e579cb90a3c82a3fdf4426bd732d40ea2451
root <lseirina87@gmail.com>
2025-07-26 11:30:08 +0300

    Added test for retrieving tags

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -18,8 +18,8 @@ def create_user(email='test@example.com', password='testpass123'):
         email=email,
         password=password
     )
-    
-    
+
+
 class PublicTagsApiTests(TestCase):
     """Tests for unauthorized user."""
     def setUp(self):
@@ -30,3 +30,28 @@ class PublicTagsApiTests(TestCase):
         res = self.client(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+        
+class PrivateTagsTests(TestCase):
+    """Tests for authenticated requests."""
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        
+    def test_retrieve_tags(self):
+        """Test retrieving tags successfull."""
+        Tag.objects.create(
+            user=self.user,
+            name='Vegan'
+        )
+        Tag.objects.create(
+            user=self.user,
+            name='Desert'
+        )
+        res = self.client.get(TAGS_URL)
+        tags = Tag.objects.all()
+        serializer = TagSerializer(tags, many=True)
+        
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
