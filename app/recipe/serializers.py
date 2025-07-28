9bbe0ecbe1b7f8bf1245bc05222e1a9918a69813
root <lseirina87@gmail.com>
2025-07-28 14:48:14 +0300

    Added update recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,10 +26,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'tags']
         read_only_field = ['id']
 
-    def create(self, validated_data):
-        """Create and return recipe with tags."""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tag(self, recipe, tags):
+        """Handling getting or creating tag as needed."""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -37,8 +35,27 @@ class RecipeSerializer(serializers.ModelSerializer):
                 name=tag['name']
             )
             recipe.tags.add(tag_obj)
-            
+
+    def create(self, validated_data):
+        """Create and return recipe with tags."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tag(recipe, tags)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create(instance, tags)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
