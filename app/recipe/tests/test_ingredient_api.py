673486f9f049d150bc5d4ad028bfe9551caf41e4
root <lseirina87@gmail.com>
2025-08-01 14:16:17 +0300

    Added test filter ing by assigned to ecipe

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -1,4 +1,6 @@
 """"Tests for ingredient API"""
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -6,7 +8,10 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Ingredient
+from core.models import (
+    Ingredient,
+    Recipe
+    )
 from recipe.serializers import IngredientSerializer
 
 
@@ -88,3 +93,22 @@ class PrivateIngredientApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         ingredients = Ingredient.objects.filter(user=self.user)
         self.assertFalse(ingredients.exists())
+
+    def test_filter_ingredient_by_assign_recipe(self):
+        """Test filtering ingredients by those assinged to recipe."""
+        ing1 = Ingredient.objects.create(user=self.user, name='Apples')
+        ing2 = Ingredient.objects.create(user=self.user, name='Turkey')
+        recipe = Recipe.objects.create(
+            user=self.user,
+            title='Apples Crumble',
+            time_minutes=20,
+            price=Decimal('3.49')
+        )
+        recipe.ingredients.add(ing1)
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        s1 = IngredientSerializer(ing1)
+        s2 = IngredientSerializer(ing2)
+
+        self.assertEqaul(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertNotIn(s2.data, res.data)
